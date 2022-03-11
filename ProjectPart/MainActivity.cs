cb4292a83d1010d1c1f4e7fe2bbab62870b4d490
jay.patel <jay.patel@1R-80>
2022-03-11 16:28:33 +0530

    Add project files.

---
+++ ProjectPart/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace ProjectPart
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ImageView imageViewBack;
+        EditText editUser, editPassword;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            // SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.Profile);
+
+            imageViewBack = FindViewById<ImageView>(Resource.Id.imageViewBackImg);
+
+                   
+
+        }
+        
+    }
+}
