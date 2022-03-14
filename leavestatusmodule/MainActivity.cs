6152187cf1d57de6207217675fbbfe9ea9751589
sagar.khati <sagar.khati@1R-81>
2022-03-14 17:49:14 +0530

    new commit

--- leavestatusmodule/MainActivity.cs
+++ leavestatusmodule/MainActivity.cs
@@ -3,12 +3,18 @@ using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
 using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
 
 namespace leavestatusmodule
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.noActionbar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        RecyclerView.LayoutManager mLayoutManager;
+       
+        List<internsonleave> leavelist;
+        recyclerviewadapter mAdapter;
         RecyclerView recycler;
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -17,7 +23,31 @@ namespace leavestatusmodule
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
             recycler = FindViewById<RecyclerView>(Resource.Id.recyclerviewId);
+            datainput();
+          
+            mLayoutManager = new LinearLayoutManager(this);
+            recycler.SetLayoutManager(mLayoutManager);
+            mAdapter = new recyclerviewadapter(leavelist, ApplicationContext);
+            recycler.SetAdapter(mAdapter);
+
         }
+
+        private void datainput()
+        {
+            leavelist = new List<internsonleave>
+            { 
+               new internsonleave{ reason ="i have to visit a family function",day="Wed",date="16 feb",typeofleave="half day",seperatordate =" feb 2022",statusofleave="accepted",buttontext="Revoke"},
+               new internsonleave{ reason ="i have to visit a dentist ",day="Wed",date="14 feb",typeofleave="full day",statusofleave="pending",buttontext="Req to revoke"},
+               new internsonleave{ reason ="i missed the bus",day="thu",date="17 feb",typeofleave="half day",seperatordate ="",statusofleave="accepted",buttontext="Revoke"},
+               new internsonleave{ reason ="i missed the train",day="Zumma",date="18 feb",typeofleave="full day",seperatordate =" feb 2032",statusofleave="pending",buttontext="Revoke"},
+               new internsonleave{ reason ="Mood nahi hai",day="Monday",date="20 feb",typeofleave="half day",seperatordate =" feb 2032",statusofleave="accepted",buttontext="Req to revoke"},
+               new internsonleave{ reason ="Shadi thi",day="Tue",date="25 feb",typeofleave="full day",seperatordate ="",statusofleave="accepted",buttontext="Revoke"},
+               new internsonleave{ reason ="Dadi nahi  rahi",day="Wed",date="26 feb",typeofleave="full day",seperatordate =" feb 2032",statusofleave="pending",buttontext="Revoke"},
+               new internsonleave{ reason ="Baju wala nahi raha",day="Thu",date="27 feb",typeofleave="full day",seperatordate ="",statusofleave="accepted",buttontext="Revoke"},
+
+            };
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- leavestatusmodule/recyclerviewadapter.cs
+++ leavestatusmodule/recyclerviewadapter.cs
@@ -10,57 +10,122 @@ namespace leavestatusmodule
 {
     class recyclerviewadapter : RecyclerView.Adapter
     {
-
+        RecyclerView.ViewHolder viewHolder;
+        public const  int USER = 0, IMAGE = 1;
+        List<internsonleave> leavelist;
+        MyViewHolder viewholder1;
+        MyViewHolder2 viewHolder2;
         Context con;
-        private readonly string[] spaceCrafts;
-        public event EventHandler<int> itemClick;
-        public List<string> employeenames;
 
-        public recyclerviewadapter(List<string> employeenames, Context context)
+        //public event EventHandler<int> itemClick;
+
+
+        public recyclerviewadapter(List<internsonleave> leavelist, Context context)
         {
-            this.employeenames = employeenames;
+            this.leavelist = leavelist;
             con = context;
         }
 
         //public override int ItemCount => throw new NotImplementedException();
         public override int ItemCount
         {
-            get { return employeenames.Count; }
+            get { return leavelist.Count; }
         }
 
 
         public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
         {
-            MyViewHolder vh = holder as MyViewHolder;
-
-            vh.textView.Text = employeenames[position].ToString();
-            vh.textView.Click += delegate {
-                Toast.MakeText(con, "message", ToastLength.Long).Show();
-            };
+            switch (holder.ItemViewType)
+            { 
+            case USER:
+                    MyViewHolder viewHolder1 = holder as MyViewHolder;
+                    configureViewHolder1(viewHolder1, position);
+                    break;
+                case IMAGE:
+                    MyViewHolder2 viewholder2 = holder as MyViewHolder2;
+                    configureViewHolder2(viewholder2, position);
+                    break;
+            }
+            //MyViewHolder vh1 = holder as MyViewHolder;
+           
+            //vh1.reaasonTextView.Text = leavelist[position].reason;
+            //vh1.dayDateTextview.Text = leavelist[position].date;
+            //vh1.typeofleaveTextview.Text = leavelist[position].typeofleave;
+            //vh1.leavestatusbutton.Text = leavelist[position].statusofleave;
+            //vh1.revokebutton.Text = leavelist[position].buttontext;
+            //MyViewHolder2 vh2 = holder as MyViewHolder2;
+            //vh2.monthnameTextView.Text = leavelist[position].seperatordate;
         }
 
+
+
+        private void configureViewHolder2(MyViewHolder2 vh2, int position)
+        {
+            vh2.monthnameTextView.Text = leavelist[position].seperatordate;
+            vh2.monthtext2.Text = leavelist[position].day;
+        }
+
+        private void configureViewHolder1(MyViewHolder vh1, int position)
+        {
+            vh1.reaasonTextView.Text = leavelist[position].reason;
+            vh1.dayDateTextview.Text = leavelist[position].date;
+            vh1.typeofleaveTextview.Text = leavelist[position].typeofleave;
+            vh1.leavestatusbutton.Text = leavelist[position].statusofleave;
+            vh1.revokebutton.Text = leavelist[position].buttontext;
+            
+        }
+
+        public override int GetItemViewType(int position)
+        {
+            if(leavelist[position].seperatordate != "" )
+                return USER;
+            else
+                return IMAGE;
+
+        }
 
 
         public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
         {
-            View itemView = LayoutInflater.From(parent.Context).
-                Inflate(Resource.Layout.hetrogenousRowitem, parent, false);
-            MyViewHolder vh = new MyViewHolder(itemView);
-            return vh;
+         
+            switch (viewType)
+            {
+                case USER:
+                    View v1 = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.hetrogenousRowitem, parent, false);
+                    viewHolder = new MyViewHolder(v1);
+                    break;
+                case IMAGE:
+                    View v2 = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.hetrogenouslayout2, parent, false);
+                    viewHolder = new MyViewHolder2(v2);
+                    break;
+            }
+            return viewHolder;
         }
-
-
-
         public class MyViewHolder : RecyclerView.ViewHolder
         {
-            private readonly Action<int> listener;
-            public TextView textView;
+            //private readonly Action<int> listener;
+            public Button leavestatusbutton,revokebutton;
+            public TextView reaasonTextView,dayDateTextview,typeofleaveTextview;
             public MyViewHolder(View itemView) : base(itemView)
             {
+                reaasonTextView = itemView.FindViewById<TextView>(Resource.Id.textviewitem1);
+                dayDateTextview = itemView.FindViewById<TextView>(Resource.Id.textviewitem2);
+                typeofleaveTextview = itemView.FindViewById<TextView>(Resource.Id.textviewitem3);
+                leavestatusbutton = itemView.FindViewById<Button>(Resource.Id.textviewitem4);
+                revokebutton = itemView.FindViewById<Button>(Resource.Id.textviewitem5);
+            
+            }
 
-       
-
-
+          
+        }
+        public class MyViewHolder2 : RecyclerView.ViewHolder
+        {
+            //private readonly Action<int> listener;
+            public TextView monthnameTextView,monthtext2;
+            public MyViewHolder2(View itemView) : base(itemView)
+            {
+                monthnameTextView = itemView.FindViewById<TextView>(Resource.Id.monthid);
+                monthtext2 = itemView.FindViewById<TextView>(Resource.Id.monthid2);
             }
 
 
