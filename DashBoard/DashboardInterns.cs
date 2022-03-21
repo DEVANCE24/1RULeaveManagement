d14d0f39229f189e233fc0b413f0e587e4b502d4
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-03-21 12:14:35 +0530

    Add project files.

---
+++ DashBoard/DashboardInterns.cs
@@ -0,0 +1,68 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.BottomNavigation;
+using static Google.Android.Material.BottomNavigation.BottomNavigationView;
+using Fragment = AndroidX.Fragment.App.Fragment;
+
+namespace DashBoard
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class DashboardInterns : AppCompatActivity, IOnNavigationItemSelectedListener
+    {
+        private BottomNavigationView _bottomNavigationViewInterns;
+        private DashboardInternsFragment _dasboardInternsFragment;
+        private StatusInternFragment _statusInternFragment;
+        private ProfileInternFragment _profileInternFragment;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.dashboardInternsLayout);
+
+            
+            _bottomNavigationViewInterns = FindViewById<BottomNavigationView>(Resource.Id.bottonNavigationViewInterns);
+
+
+            _bottomNavigationViewInterns.SetOnNavigationItemSelectedListener(this);
+            _dasboardInternsFragment = new DashboardInternsFragment(this);
+            _statusInternFragment = new StatusInternFragment();
+            _profileInternFragment = new ProfileInternFragment();
+            SupportFragmentManager.BeginTransaction().Replace(Resource.Id.frameLayoutDashBoard, _dasboardInternsFragment).Commit();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            
+            switch (item.ItemId)
+            {
+                case Resource.Id.dashboard:
+                    SupportFragmentManager.BeginTransaction().Replace(Resource.Id.frameLayoutDashBoard, _dasboardInternsFragment).Commit();
+                    break;
+
+                case Resource.Id.status:
+                    SupportFragmentManager.BeginTransaction().Replace(Resource.Id.frameLayoutDashBoard, _statusInternFragment).Commit();
+                    break;
+
+                case Resource.Id.profile:
+                    SupportFragmentManager.BeginTransaction().Replace(Resource.Id.frameLayoutDashBoard, _profileInternFragment).Commit();
+                    break;
+
+
+            }
+
+          
+
+            return true;
+        }
+    }
+}
---
+++ DashBoard/DashboardInternsFragment.cs
@@ -0,0 +1,51 @@
+﻿
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fragment = AndroidX.Fragment.App.Fragment;
+
+namespace DashBoard
+{
+
+
+    class DashboardInternsFragment : Fragment
+    {
+
+        private RecyclerView _recyclerViewHolidayList;
+        private RecyclerView.LayoutManager _layoutmanager;
+        private HolidayListAdapter _holidayListAdapter;
+        private HolidayList _holidaylist;
+        private Context context;
+
+        public DashboardInternsFragment(Context context)
+        {
+            this.context= context;
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.dashboardInternsFragmentLayout, container, false);
+            _recyclerViewHolidayList = view.FindViewById<RecyclerView>(Resource.Id.recyclerViewHolidayList);
+
+            _recyclerViewHolidayList.AddItemDecoration(new DividerItemDecoration(Activity.ApplicationContext, DividerItemDecoration.Vertical));
+            
+            _layoutmanager = new LinearLayoutManager(context);
+            _recyclerViewHolidayList.SetLayoutManager(_layoutmanager);
+            _holidaylist = new HolidayList();
+
+            _holidayListAdapter = new HolidayListAdapter(_holidaylist, context);
+            _recyclerViewHolidayList.SetAdapter(_holidayListAdapter);
+            
+            return view;
+
+          
+        }
+    }
+}
