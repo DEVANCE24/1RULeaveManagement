b5ace5bddf6a3d1faf530ecc11eecb6e2807e72f
priyank.patel <priyank.patel@1R-38>
2022-03-23 15:51:51 +0530

    change pass

---
+++ tp_and/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+
+namespace tp_and
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.forgetpassword);
+        }
+    }
+}
