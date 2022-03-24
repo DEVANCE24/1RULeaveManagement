c4de8895feae3a2cc6bbffb1fac5496705a720c0
jay.patel <jay.patel@1R-80>
2022-03-24 14:25:42 +0530

    ApplyLeave

--- ProjectPart/MainActivity.cs
+++ ProjectPart/MainActivity.cs
@@ -6,8 +6,10 @@ using Android.Widget;
 using AndroidX.AppCompat.App;
 using AndroidX.Fragment.App;
 using AndroidX.ViewPager.Widget;
+using Felipecsl.GifImageViewLibrary;
 using Google.Android.Material.Tabs;
 using Java.Lang;
+using ProjectPart.dialogFragments;
 using System;
 using ActionBar = Android.App.ActionBar;
 
@@ -17,9 +19,16 @@ namespace ProjectPart
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        TabLayout _mytabLayout;
-        TextView _mytextView;
-        ViewPager _myViewPager;
+        public TabLayout _mytabLayout;
+        public TextView _mytextView;
+        public FrameLayout frameLayout;
+        public ImageView _myImage;
+        public Button _myApplyButton;
+        public GifImageView myGIFImage;
+
+        AndroidX.Fragment.App.Fragment fragment;
+        AndroidX.Fragment.App.Fragment fragment1;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -27,28 +36,89 @@ namespace ProjectPart
             SetContentView(Resource.Layout.ApplyLeave);
 
             _mytabLayout = FindViewById<TabLayout>(Resource.Id.tabLayout);
-            _mytextView = FindViewById<TextView>(Resource.Id.textViewName);
-            _myViewPager = FindViewById<ViewPager>(Resource.Id.viewPagerEx);
+            _myApplyButton = FindViewById<Button>(Resource.Id.applyButton);
+            //myGIFImage = FindViewById<GifImageView>(Resource.Id.myGIFImage);
+            //_mytabLayout.TabSelected += _myTabSelected;
+            UIReferences();
+            UIClick();
 
-           
-
-            _mytabLayout.TabSelected += _myTabSelected;
-            
             setTabName();
-                     
-        }
+            fragment1 = new fragmentOne();
+            SupportFragmentManager.BeginTransaction().Replace(Resource.Id.frameLayoutEx, fragment1).Commit();
+            _mytabLayout.TabSelected += (object sender, TabLayout.TabSelectedEventArgs e) =>
+            {
+                AndroidX.Fragment.App.Fragment fragment = null;
+                var tab = e.Tab;
+               
+                switch (tab.Position)
+                {
+                    case 0:
+                        fragment = new fragmentOne();
+                        break;
+                    case 1:
+                        fragment = new fragmentTwo();
+                        break;
+                }
+                SupportFragmentManager.BeginTransaction().Replace(Resource.Id.frameLayoutEx, fragment).Commit();
+             
+            };
 
-        private void _myTabSelected(object sender, TabLayout.TabSelectedEventArgs e)
-        {
-            Fragment1 frag = new Fragment1();
-            var trans = SupportFragmentManager.BeginTransaction();
-            frag
         }
         private void setTabName()
         {
             _mytabLayout.AddTab(_mytabLayout.NewTab().SetText(Resource.String.leave));
             _mytabLayout.AddTab(_mytabLayout.NewTab().SetText(Resource.String.workFromHome));
 
-        }       
-    }  
+        }
+
+        private void UIReferences()
+        {
+            _myImage = FindViewById<ImageView>(Resource.Id.imageViewBack);
+            
+        }
+
+        private void UIClick()
+        {
+            _myImage.Click += Back_Click;
+            _myApplyButton.Click += _myApplyButton_Click;
+        }
+
+        private void _myApplyButton_Click(object sender, EventArgs e)
+        {
+            loginSuccessful loginFrag = new loginSuccessful();
+            var frag = SupportFragmentManager.BeginTransaction();
+            loginFrag.Cancelable = true;
+            loginFrag.Show(frag, "loginFrag");
+
+            /* myGIFImage= FindViewById<GifImageView>(Resource.Id.myGIFImage);
+    // From Drawable
+                Stream input = Resources.OpenRawResource(Resource.Drawable.loading);
+                
+                // You should convert the "input" into Byte Array 
+                byte[] bytes = ConvertByteArray(input);
+                
+                myGIFImage.SetBytes(bytes);
+                myGIFImage.StartAnimation();
+}
+private byte[] ConvertByteArray(Stream input)
+                {
+                    byte[] buffer = new byte[16 * 1024];
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        int read;
+                        while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                            ms.Write(buffer, 0, read);
+                        return ms.ToArray();
+                    }
+                }*/
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            //Intent i = new Intent(this,typeof());
+            //StartActivity(i);
+        }
+
+        
+    }
 }
--- ProjectPart/fragmentTwo.cs
+++ ProjectPart/fragmentTwo.cs
@@ -1,57 +1,62 @@
-﻿using Android.App;
+﻿
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
+using Android.Views;
 using Android.Widget;
-using AndroidX.AppCompat.App;
-using Java.Lang;
+using AndroidX.Fragment.App;
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace ProjectPart
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
-    public class WorkFromHome : AppCompatActivity
+    public class fragmentTwo : Fragment
     {
         TextView textViewOne, textViewTwo;
         ImageView imageViewOne, imageViewTwo;
         private DateFromPickerDialoguefragment dateFromPickerDialoguefragment;
         private DateToPickerDialoguefragment dateToPickerDialoguefragment;
         private readonly string _tag = "Main Activity";
-
-        protected override void OnCreate(Bundle savedInstanceState)
+        public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.WorkFromHomeLayout);
 
-            imageViewOne = FindViewById<ImageView>(Resource.Id.imageViewFromCalender);
-            textViewOne = FindViewById<TextView>(Resource.Id.textViewShowDateFrom);
+            // Create your fragment here
+        }
 
-            imageViewTwo = FindViewById<ImageView>(Resource.Id.imageViewToCalender);
-            textViewTwo = FindViewById<TextView>(Resource.Id.textViewShowDateTo);
-            
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            View view = inflater.Inflate(Resource.Layout.WorkFromHomeLayout, container, false);
+            imageViewOne =view.FindViewById<ImageView>(Resource.Id.imageViewFromCalender);
+            textViewOne = view.FindViewById<TextView>(Resource.Id.textViewShowDateFrom);
 
+            imageViewTwo = view.FindViewById<ImageView>(Resource.Id.imageViewToCalender);
+            textViewTwo = view.FindViewById<TextView>(Resource.Id.textViewShowDateTo);
 
-            imageViewOne.Click += FromDatePicker;
-            imageViewTwo.Click += ToDatePicker;
             ObjectCreation();
             BindEventofDateChange();
 
-
+            imageViewOne.Click += FromDatePicker;
+            imageViewTwo.Click += ToDatePicker;
+           
+            return view;
         }
-
         private void FromDatePicker(object sender, EventArgs e)
         {
-            dateFromPickerDialoguefragment.Show(SupportFragmentManager, _tag);
-            
+            dateFromPickerDialoguefragment.Show(Activity.SupportFragmentManager, _tag);
+
         }
 
         private void ToDatePicker(object sender, EventArgs e)
         {
-            dateToPickerDialoguefragment.Show(SupportFragmentManager, _tag);
+            dateToPickerDialoguefragment.Show(Activity.SupportFragmentManager, _tag);
         }
 
-        
+
         private void ObjectCreation()
         {
             dateFromPickerDialoguefragment = new DateFromPickerDialoguefragment();
@@ -63,15 +68,14 @@ namespace ProjectPart
             dateToPickerDialoguefragment.OnDateChangeHandler += DatePickerDialoguefragment_ToDateChangeHandler;
         }
 
-        private void DatePickerDialoguefragment_FromDateChangeHandler(object sender,DateTime e)
+        private void DatePickerDialoguefragment_FromDateChangeHandler(object sender, DateTime e)
         {
             textViewOne.Text = e.ToString(format: "dd/MM/yyyy");
         }
         private void DatePickerDialoguefragment_ToDateChangeHandler(object sender, DateTime e)
         {
-            
+
             textViewTwo.Text = e.ToString(format: "dd/MM/yyyy");
         }
-
     }
 }
