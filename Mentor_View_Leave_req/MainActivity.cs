bd198a677d35f03ffc8a255be02cbe247b6c9098
vrutti lad <vruttilad2000@gmail.com>
2022-03-24 14:51:17 +0530

    updated

--- Mentor_View_Leave_req/MainActivity.cs
+++ Mentor_View_Leave_req/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace Mentor_View_Leave_req
@@ -8,6 +9,8 @@ namespace Mentor_View_Leave_req
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        ImageView _arrowback;
+        TextView _name, _from, _to, _date, _type, _viewleaverequest;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
